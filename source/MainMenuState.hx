a5e9ff2400039d69ccc0e54dfe7efcee7124e164
Maykoll Youtube <97548071+MAYKOLLofyt@users.noreply.github.com>
2022-06-10 22:46:35 -0500

    Oooooohhhhhhhh fuckkkkk

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -35,10 +35,9 @@ class MainMenuState extends MusicBeatState
 	var optionShit:Array<String> = [
 		'story_mode',
 		'freeplay',
-		#if MODS_ALLOWED 'mods', #end
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
+		
 		'credits',
-		#if !switch 'donate', #end
+		
 		'options'
 	];
 
@@ -239,11 +238,8 @@ class MainMenuState extends MusicBeatState
 										MusicBeatState.switchState(new StoryMenuState());
 									case 'freeplay':
 										MusicBeatState.switchState(new FreeplayState());
-									#if MODS_ALLOWED
-									case 'mods':
-										MusicBeatState.switchState(new ModsMenuState());
-									#end
-									case 'awards':
+									
+									case 'extras':
 										MusicBeatState.switchState(new AchievementsMenuState());
 									case 'credits':
 										MusicBeatState.switchState(new CreditsState());
