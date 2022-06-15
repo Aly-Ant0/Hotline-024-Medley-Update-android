09679e83fd8e9442e60f46af3e759b9c5ae4f944
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-06-15 06:06:04 -0300

    add flash event

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -94,6 +94,7 @@ class ChartingState extends MusicBeatState
 		['Change Character', "Value 1: Character to change (Dad, BF, GF)\nValue 2: New character's name"],
 		['Change Scroll Speed', "Value 1: Scroll Speed Multiplier (1 is default)\nValue 2: Time it takes to change fully in seconds."],
 		['Set Property', "Value 1: Variable name\nValue 2: New value"]
+		['Flash', "Value 1: nothing, Value 2: nothing to lol"]
 	];
 
 	var _file:FileReference;
