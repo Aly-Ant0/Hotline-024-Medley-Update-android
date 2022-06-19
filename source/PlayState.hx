cf21021be9e758b4ba921be64991245865c3555f
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-06-19 01:26:29 -0300

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4561,7 +4561,7 @@ class PlayState extends MusicBeatState
 		switch(comboArray)
 		{
 			case 'perfect':
-				else if (combo => 10)
+				if (combo => 10)
 				{
 					combotxt1 new FlxText(0, healthBarBG.y + 36, FlxG.width, "perfect!", 32);
 					combotxt1.setFormat(Paths.font("goodbyeDespair.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
@@ -4597,7 +4597,7 @@ class PlayState extends MusicBeatState
 					});
 				}
 			case 'bruh': // whoops...
-				else if (songMisses > 1)
+				if (songMisses > 1)
 				{
 					combotxt3 new FlxText(0, healthBarBG.y + 36, FlxG.width, "Whoops...", 32);
 					combotxt3.setFormat(Paths.font("goodbyeDespair.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
