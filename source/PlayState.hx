efd1cf7091fccb22febead553661bca43276b883
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-06-19 16:41:02 -0300

    eu nao percebi isso tmnc

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1643,7 +1643,7 @@ class PlayState extends MusicBeatState
 		add(botplayTxt);
 
 		// lol
-		combotxt1 new FlxText();
+		combotxt1 = new FlxText();
 		combotxt1.size = 32;
 		combotxt1.color = FlxColor.WHITE
 		combotxt1.x = 300;
@@ -1654,7 +1654,7 @@ class PlayState extends MusicBeatState
 		add(combotxt1);
 
 		// combo score +
-		combotxt2 new FlxText(0, combotxt1.y + 15, 0, lerpScore, 26);
+		combotxt2 = new FlxText(0, combotxt1.y + 15, 0, lerpScore, 26);
 		combotxt2.setFormat(Paths.font("goodbyeDespair.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		combotxt2.scrollFactor.set();
 		combotxt2.borderSize = 1.25;
