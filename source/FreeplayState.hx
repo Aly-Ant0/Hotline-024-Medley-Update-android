3847480eb4dde58968a61a76e25ac5e2f5c89db8
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-06-20 14:36:42 -0300

    i fogor

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -501,6 +501,7 @@ class SongMetadata
 	public var songName:String = "";
 	public var week:Int = 0;
 	public var songCharacter:String = "";
+	public var color:Int = -7179779;
 	public var folder:String = "";
 
 	public function new(song:String, week:Int, songCharacter:String, color:Int)
