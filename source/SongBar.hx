4e57d5672a1326af58ad3a9d665c1efe068e1d19
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-07-07 23:03:42 -0300

    add song bar haxe file

---
+++ source/SongBar.hx
@@ -0,0 +1,38 @@
+import flixel.FlxSprite;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import sys.io.File;
+import sys.FileSystem;
+
+class SongBar extends FlxSprite
+{
+	var bar:FlxSprite;
+	var songTxt:FlxText;
+
+	override function create() {
+		bar = new FlxSprite().makeGraphic(1, 90, FlxColor.BLACK);
+		bar.alpha = 0.34;
+		bar.scale.x = FlxG.width - songTxt.x + 15;
+		bar.x = FlxG.width - (bar.scale.x / 2);
+		add(bar);
+
+		songTxt = new FlxText(FlxG.width + 10, bar.y + 9, 0, "", 37);
+		songTxt.setFormat(Paths.font("LEMONMILK-Bold.otf"), 32, FlxColor.WHITE, RIGHT);
+		add(songTxt);
+		super.create();
+	}
+
+	override function update(elapsed:Float) {
+		var songName:String = Paths.formatToSongPath(SONG.song);
+		var directory:String = 'data/' + songName + '/';
+		var content:String = directory + 'info.txt';
+		//var get:String = c
+		if(FileSystem.exists(content)) {
+			songTxt.text = getContent(content);
+		}
+		else {
+			songTxt.text = 'null';
+		}
+		super.update(elapsed);
+	}
+}
