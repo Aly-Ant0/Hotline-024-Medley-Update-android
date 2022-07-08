7346babf4be7157809678816cd6fd22732481052
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-07-08 00:47:51 -0300

    wha

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -25,7 +25,7 @@ import flixel.addons.transition.FlxTransitionableState;
 import flixel.graphics.atlas.FlxAtlas;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.group.FlxGroup.FlxTypedGroup;
-import flixel.group.FlxTypedSpriteGroup;
+import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
 import flixel.math.FlxMath;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRect;
