5ff16269175bfad832a2db1cbb594dd71285fd84
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-07-13 02:24:07 -0300

    change stuff

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -31,12 +31,7 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"hey you, thanks for playing this recreation!\n
-			this recreation is made by\n
-			MAYKOLLYOUTUBE and Alyant\n
-			we hope you all enjoy it\n credits in the credits menu\n to the original creators \n of hotline 024! press A to play /n
-			also you can disable the shaders in graphic options",
-			30);
+			"hey you, thanks for playing this recreation!\n this recreation is made by\n MAYKOLLYOUTUBE and Aly-Ant\n we hope you all enjoy it\n credits in the credits menu\n to the original creators \n of hotline 024! press A to play /n also you can disable the shaders in graphic options /n and enable some optimization options if you have a low end phone. /n thank you.", 30);
 		warnText.setFormat("goodbyeDespair", 30, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
