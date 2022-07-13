3094104e4fb9453b6b569edb5ea517634dadc8ea
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-07-12 22:08:09 -0300

    add show the codes achiviment

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -26,7 +26,8 @@ class Achievements {
 		["Hyperactive",					"Finish a Song without going Idle.",				'hype',					false],
 		["Just the Two of Us",			"Finish a Song pressing only two keys.",			'two_keys',				false],
 		["Toaster Gamer",				"Have you tried to run the game on a toaster?",		'toastie',				false],
-		["Debugger",					"Beat the \"Test\" Stage from the Chart Editor.",	'debugger',				 true]
+		["Debugger",					"Beat the \"Test\" Stage from the Chart Editor.",	'debugger',				 true],
+		["Show all the codes",					"just type 2480 in code screen lol",	"showcodes",					true]
 	];
 	public static var achievementsMap:Map<String, Bool> = new Map<String, Bool>();
 
