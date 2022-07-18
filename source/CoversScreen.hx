eb2f411a0a327d62cb4f0dd3df1eb4d79e897394
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-07-18 14:31:32 -0300

    update CoversScreen.hx

--- source/CoversScreen.hx
+++ source/CoversScreen.hx
@@ -21,123 +21,110 @@ using StringTools;
 
 class CoversScreen extends MusicBeatState
 {
-  var buttons:FlxSprite;
-  var buttonSelected:FlxSprite;
-  var button1:FlxSprite;
-  var button2:FlxSprite;
-  var button3:FlxSprite;
-  var button4:FlxSprite;
-  var button1Black:FlxSprite;
-  var button2Black:FlxSprite;
-  var button3Black:FlxSprite;
-  var button4Black:FlxSprite;
-  var text:FlxSprite;
-  var bars:FlxSprite;
-  public var songName:String = "";
+	var buttonGrp:FlxTypedGroup<FlxSprite>;
+	var text:FlxSprite;
+	var bars:FlxSprite;
 
-  public static var curSelected:Int = 0;
+	public static var curSelected:Int = 0;
 
-  var coverList:Array<String> = [
-    'thunderstorm',
-    'cg-megamix',
-    'promenade',
-    'death-match'
-  ];
+	var buttonList:Array<String> = [
+		'button1',
+		'button2',
+		'button3',
+		'button4'
+	];
   
-  var buttonList:Array<String> = [
-    'button1',
-    'button2',
-    'button3',
-    'button4'
-    ];
-  
-  override function create()
-  {
-    Paths.clearStoredMemory();
+	override function create()
+	{
+		Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
 		PlayState.noSkins = false;
 
-    text = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/text'));
-    text.screenCenter();
-    
-    var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/bg'));
-    bg.screenCenter();
-    
-    bars = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/bars2'));
-    bars.screenCenter();
-    
-    /*buttons = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/' + buttonList));
-    buttons.screenCenter();
-    
-    buttonSelected = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/' + buttonList[curSelected]));
-    buttonSelected.screenCenter();*/ //idk
-    
-    button1 = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button1'));
-    button1.screenCenter();
-    
-    button2 = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button2'));
-    button2.screenCenter();
-    
-    button3 = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button3'));
-    button3.screenCenter();
-    
-    button4 = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button4'));
-    button4.screenCenter();
-   
-    button1Black = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button1'));
-    button1Black.color = FlxColor.BLACK;
-    button1.screenCenter();
-    
-    button2Black = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button2'));
-    button2Black.color = FlxColor.BLACK;
-    button2.screenCenter();
-    
-    button3Black = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button3'));
-    button3Black.color = FlxColor.BLACK;
-    button3Black.screenCenter();
-    
-    button4Black = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button4'));
-    button4Black.color = FlxColor.BLACK;
-    button4Black.screenCenter();
-    
-    add(bg);
-    add(bars);
-    add(text);
-    add(button1Black);
-    add(button2Black);
-    add(button3Black);
-    add(button4Black);
-    add(button1);
-    add(button2);
-    add(button3);
-    add(button4);
-    //add(buttonSelected);
-    
-    changeCover();
-    super.create();
-    
-    #if android
-    addVirtualPad(UP_DOWN, A_B);
-    #end
-  }
-  override function update(elapsed:Float)
-  {
-    if (controls.UI_UP_P)
-    {
-      FlxG.sound.play(Paths.sound('selectsfx'));
-      changeCover(-1);
-    }
-    if (controls.UI_DOWN_P)
-    {
-      FlxG.sound.play(Paths.sound('selectsfx'));
-      changeCover(1);
-    }
-    if (controls.BACK)
-    {
-      MusicBeatState.switchState(new ExtrasScreen());
-    }
-    if (controls.ACCEPT)
-    {
+		text = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/text'));
+		text.screenCenter();
+
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/bg'));
+		bg.screenCenter();
+
+		bars = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/bars2'));
+		bars.screenCenter();
+
+		buttonGrp = new FlxTypedGroup<FlxSprite>();
+
+		for (i in 0...4) {
+			var butt:FlxSprite = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button' + i));
+			butt.screenCenter();
+			butt.ID = i;
+			buttonGrp.add(butt);
+		}
+		/*button1 = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button1'));
+		button1.screenCenter();
+
+		button2 = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button2'));
+		button2.screenCenter();
+
+		button3 = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button3'));
+		button3.screenCenter();
+
+		button4 = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button4'));
+		button4.screenCenter();
+
+		button1Black = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button1'));
+		button1Black.color = FlxColor.BLACK;
+		button1.screenCenter();
+
+		button2Black = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button2'));
+		button2Black.color = FlxColor.BLACK;
+		button2.screenCenter();
+
+		button3Black = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button3'));
+		button3Black.color = FlxColor.BLACK;
+		button3Black.screenCenter();
+
+		button4Black = new FlxSprite().loadGraphic(Paths.image('hotline/menu/covers/button4'));
+		button4Black.color = FlxColor.BLACK;
+    button4Black.screenCenter();*/
+
+		add(bg);
+		add(bars);
+		add(text);
+		add(buttonGrp);
+		//add(button1Black);
+		//add(button2Black);
+		//add(button3Black);
+		//add(button4Black);
+		//add(button1);
+		//add(button2);
+		//add(button3);
+		//add(button4);
+		//add(buttonSelected);
+
+		changeCover();
+		super.create();
+
+		#if android
+		addVirtualPad(UP_DOWN, A_B);
+		#end
+	}
+	override function update(elapsed:Float)
+	{
+		if (controls.UI_UP_P)
+		{
+			FlxG.sound.play(Paths.sound('selectsfx'));
+			changeCover(-1);
+		}
+		if (controls.UI_DOWN_P)
+		{
+			FlxG.sound.play(Paths.sound('selectsfx'));
+			changeCover(1);
+		}
+		if (controls.BACK)
+		{
+			FlxG.sound.play(Paths.sound('backsfx'));
+			MusicBeatState.switchState(new ExtrasScreen());
+		}
+		if (controls.ACCEPT)
+		{
 			var coverChoice:String = buttonList[curSelected];
 			switch(coverChoice)
 			{
@@ -178,77 +165,30 @@ class CoversScreen extends MusicBeatState
 		super.update(elapsed);
 	}
 
-  function changeCover(change:Int = 0)
-  {
-    curSelected += change;
-    
-        if (curSelected < 0)
+	function changeCover(change:Int = 0)
+	{
+		curSelected += change;
 
+		if (curSelected < 0)
 			curSelected = buttonList.length - 1;
 
 		if (curSelected >= buttonList.length)
 			curSelected = 0;
-			
-		switch(curSelected)
-		{
-		  case 0:
-		  button1.alpha = 1;
-		  button1Black.alpha = 0;
-		  //button2.color = FlxColor.BLACK;
-		  button2.alpha = 0.25;
-		  button2Black.alpha = 0.75;
-		  //button3.color = FlxColor.BLACK;
-		  button3.alpha = 0.25;
-		  button3Black.alpha = 0.75;
-		  //button4.color = FlxColor.BLACK;
-		  button4.alpha = 0.25;
-		  button4Black.alpha = 0.75;
-		  case 1:
-		  //button1.color = FlxColor.BLACK;
-		  button1.alpha = 0.25;
-		  button1Black.alpha = 0.75;
-		  button2.alpha = 1;
-		  button2Black.alpha = 0;
-		  //button3.color = FlxColor.BLACK;
-		  button3.alpha = 0.25;
-		  button3Black.alpha = 0.75;
-		  //button4.color = FlxColor.BLACK;
-		  button4Black.alpha = 0.75;
-		  button4.alpha = 0.25;
-		  case 2:
-		  //button1.color = FlxColor.BLACK;
-		  button1.alpha = 0.25;
-		  button1Black.alpha = 0.75;
-		  //button2.color = FlxColor.BLACK;
-		  button2.alpha = 0.25;
-		  button2Black.alpha = 0.75;
-		  button3.alpha = 1;
-		  button3Black.alpha = 0;
-		  //button4.color = FlxColor.BLACK;
-		  button4.alpha = 0.25;
-		  button4Black.alpha = 0.75;
-		  case 3:
-		  //button1.color = FlxColor.BLACK;
-		  button1.alpha = 0.25;
-		  button1Black.alpha = 0.75;
-		  //button2.color = FlxColor.BLACK;
-		  button2.alpha = 0.25;
-		  button2Black.alpha = 0.75;
-		  //button3.color = FlxColor.BLACK;
-		  button3.alpha = 0.25;
-		  button3Black.alpha = 0.75;
-		  button4.alpha = 1;
-		  button4Black.alpha = 0;
-		}
-		/*var coverSelected:FlxGraphic = Paths.image('hotline/menu/covers/' + buttonList[curSelected]);
-		
-		var cover:FlxGraphic = Paths.image('hotline/menu/covers/' + buttonList);
-		
-			for (i in 0...buttonList.length)
-			{
-				buttons.alpha = 0.48;
-			}
 
-			buttons.alpha = 1;*/
-  }
+		/*var coverSelected:FlxGraphic = Paths.image('hotline/menu/covers/' + buttonList[curSelected]);
+
+		var cover:FlxGraphic = Paths.image('hotline/menu/covers/' + buttonList);*/
+
+			for (butt in buttonGrp.members)
+			{
+				butt.alpha = 0.65;
+				butt.color = 0xFF363636;
+
+				if(butt.ID == curSelected) {
+					butt.alpha = 1;
+					butt.color = 0xFFFFFFFF;
+					FlxTween.tween(butt, {y: butt.y + 3}, 1, {ease: FlxEase.expoOut, type:PINGPONG});
+				}
+			}
+	}
 }
