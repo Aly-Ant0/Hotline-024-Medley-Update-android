84d8a677a1495db79b1784e88c44702f97f1f5ac
Maykoll Youtube <97548071+MAYKOLLofyt@users.noreply.github.com>
2022-07-30 10:55:18 -0500

    Bruh

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -278,6 +278,8 @@ class TitleState extends MusicBeatState
 		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24, false);
 		logoBl.animation.play('bump');
 		logoBl.updateHitbox();
+		
+    logoBl.screenCenter(X);
 		// logoBl.screenCenter();
 		// logoBl.color = FlxColor.BLACK;
 
