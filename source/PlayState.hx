83705d135a414367cac3b910221ad81d5ca7ee48
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-07-31 20:38:48 -0300

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1706,7 +1706,7 @@ class PlayState extends MusicBeatState
 				bfshade.blend = ADD;
 				bfshade.alpha = 0.5;
 				bfshade.y += boyfriend.y;
-				bfshade.animation.play(boyfriend.curAnim);
+				bfshade.animation.play(boyfriend.AnimName);
 				insert(members.indexOf(boyfriendGroup), bfshade);
 			case 'limo':
 				resetFastCar();
