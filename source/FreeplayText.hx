1d176bff3ec129cb70dba313e746a74cb54b907f
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-08-27 17:08:23 -0300

    update FreeplayText.hx

--- source/FreeplayText.hx
+++ source/FreeplayText.hx
@@ -12,7 +12,13 @@ class FreeplayText extends FlxSkewedSprite
 	public var targetY:Float = 0;
 	public var forceX:Float = Math.NEGATIVE_INFINITY;
 	public var forceSkew:Float = Math.NEGATIVE_INFINITY;
+	public var notSlct:Bool = false;
+	public var select:Bool = false;
 	public var flashingInt:Int = 0;
+	var maxSkew:Float = -2; // selected item
+	var minSkew:Float = -5; // not selected item
+	var skewDirection:Int = 1; // lmao
+	var skewSpeed:Float = 0.2;
 
 	public function new(x:Float, y:Float, portName:String = '')
 	{
@@ -38,7 +44,29 @@ class FreeplayText extends FlxSkewedSprite
 				skew.x = forceSkew;
 		}
 
-		y = FlxMath.lerp(y, (targetY * 150) - -10, CoolUtil.boundTo(elapsed * 10.2, 0, 1));
+		y = FlxMath.lerp(y, (targetY * 150) - -10, CoolUtil.boundTo(elapsed * 32, 0, 1));
+
+		if(notSlct){
+			var lastAngle:Float = lastAngle;
+
+			angle = FlxMath.lerp(lastAngle, 2 + 3 * targetY, CoolUtil.boundTo(elapsed * 27, 0, 1));
+			skew.x = skewDirection * skewSpeed * elapsed;
+			forceSkew = skew.x;
+
+			if (skew.x > maxSkew){ // no caso é oq ta selecionando
+					skew.x = maxSkew;
+					skewDirection = 1;
+			}
+
+			else if (skew.x > minSkew){ // no caso é oq nao ta selecionando
+					skew.x = minSkew - 2;
+					skewDirection = -1;
+			}
+		}
+		if(select){
+			skew.x = skewDirection * skewSpeed * elapsed;
+			port.angle = FlxMath.lerp(port.angle, -1, lerpVal);
+		}
 		//x = x * (targetY - 0.5);
 	}
 }
