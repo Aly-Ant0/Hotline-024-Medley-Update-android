7347c230e29ea17acef00d225f7e954bbc7a1547
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-08-30 16:11:18 -0300

    update CodeScreen.hx

--- source/CodeScreen.hx
+++ source/CodeScreen.hx
@@ -126,7 +126,7 @@ class CodeScreen extends MusicBeatState
 			numbersSpr.add(button);
 		}
 
-		code = new FlxText(500, 120, 0, "", 34);
+		code = new FlxText(480, 270, 0, "", 34);
 		code.setFormat(Paths.font("LEMONMILK-Bold.otf"), 34, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		code.text = '';
 		//code.textField = 0.40;
