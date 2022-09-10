0607ce18a8d16fdfe424244b618b362be9a3b887
Maykoll Youtube <97548071+MAYKOLLofyt@users.noreply.github.com>
2022-09-09 22:59:32 -0500

    Testing (aly pls stop cry lol)

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -30,6 +30,9 @@ class CreditsState extends MusicBeatState
 	private var creditsStuff:Array<Array<String>> = [];
 
 	var bg:FlxSprite;
+	var gradient:FlxSprite;
+	var grd3:FlxSprite;
+	var bars:FlxSprite
 	var descText:FlxText;
 	var intendedColor:Int;
 	var colorTween:FlxTween;
@@ -45,9 +48,24 @@ class CreditsState extends MusicBeatState
 		#end
 
 		persistentUpdate = true;
-		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg = new FlxSprite().loadGraphic(Paths.image('images/menu/credits/bg'));
 		add(bg);
 		bg.screenCenter();
+		
+		persistentUpdate = true;
+		gradient = new FlxSprite().loadGraphic(Paths.image('images/menu/credits/gradient'));
+		add(gradient);
+		gradient.screenCenter();
+		
+		persistentUpdate = true;
+		grd3 = new FlxSprite().loadGraphic(Paths.image('images/menu/credits/grd3'));
+		add(grd3);
+		grd3.screenCenter();
+   
+		persistentUpdate = true;
+		bar = new FlxSprite().loadGraphic(Paths.image('images/menu/credits/bar'));
+		add(bar);
+		bar.screenCenter();
 		
 		grpOptions = new FlxTypedGroup<Alphabet>();
 		add(grpOptions);
@@ -95,26 +113,9 @@ class CreditsState extends MusicBeatState
 			['Psych Engine Android'],
 			['Saw (M.A. Jigsaw)',	'saw',				'Main Programmer of Psych Engine Android',					'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ', 	'F73838'],
 			[''],
-			['Psych Engine Team'],
-			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',							'https://twitter.com/Shadow_Mario_',	'444444'],
-			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',						'https://twitter.com/RiverOaken',		'C30085'],
-			['shubs',				'shubs',			'Additional Programmer of Psych Engine',					'https://twitter.com/yoshubs',			'279ADC'],
-			[''],
-			['Former Engine Members'],
-			['bb-panzu',			'bb-panzu',			'Ex-Programmer of Psych Engine',							'https://twitter.com/bbsub3',			'389A58'],
-			[''],
-			['Engine Contributors'],
-			['iFlicky',				'iflicky',			'Composer of Psync and Tea Time\nMade the Dialogue Sounds',	'https://twitter.com/flicky_i',			'AA32FE'],
-			['SqirraRNG',			'gedehari',			'Chart Editor\'s Sound Waveform base',						'https://twitter.com/gedehari',			'FF9300'],
-			['PolybiusProxy',		'polybiusproxy',	'.MP4 Video Loader Extension',								'https://twitter.com/polybiusproxy',	'FFEAA6'],
-			['Keoiki',				'keoiki',			'Note Splash Animations',									'https://twitter.com/Keoiki_',			'FFFFFF'],
-			['Smokey',				'smokey',			'Spritemap Texture Support',								'https://twitter.com/Smokey_5_',		'4D5DBD'],
-			[''],
-			["Funkin' Crew"],
-			['ninjamuffin99',		'ninjamuffin99',	"Programmer of Friday Night Funkin'",						'https://twitter.com/ninja_muffin99',	'F73838'],
-			['PhantomArcade',		'phantomarcade',	"Animator of Friday Night Funkin'",							'https://twitter.com/PhantomArcade3K',	'FFBB1B'],
-			['evilsk8r',			'evilsk8r',			"Artist of Friday Night Funkin'",							'https://twitter.com/evilsk8r',			'53E52C'],
-			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",							'https://twitter.com/kawaisprite',		'6475F3']
+			['Hotline 024 devs'],
+			['Sakury', 'saru',        'Director, Musician'         'https://twitter.com/Saruky__',     '2833E7'],
+			['VaChildish', 'BigBand',        'Artist, Jojo Nikku`s Assets',         'https://twitter.com/VaChildish',     'FF0069'],
 		];
 		
 		for(i in pisspoop){
