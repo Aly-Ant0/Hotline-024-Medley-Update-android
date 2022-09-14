f79c05f71d3c30be448f2ae853b06b0a20927ef1
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-09-14 01:59:37 -0300

    idk

---
+++ source/Something.hx
@@ -0,0 +1,12 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+
+class Something extends MusicBeatState{ // just a test in github editor lmao
+  var somethin:FlxSprite;
+  
+  override public function create(){
+    
+  }
+}
