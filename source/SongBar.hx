2a5540439ef0c385765b8f46f32d7d8a3b05ac23
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-11-26 12:58:11 -0300

    im makinga a mod called untitled mod

--- source/SongBar.hx
+++ source/SongBar.hx
@@ -6,27 +6,41 @@ import flixel.FlxSprite;
 import flixel.group.FlxSpriteGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
+import Song.SwagSong;
 
 using StringTools;
 using flixel.util.FlxSpriteUtil;
 
 class SongBar extends FlxSpriteGroup // culpa do bolsonaro
 {
+	var song:SwagSong;
 	var size:Float = 0;
 	var fontSize:Int = 42;
 	public var stringShit:String = "";
 
 	public function new(x:Float, y:Float) {
 		super(x, y);
+		var songName:String = Paths.formatToSongPath(song.song);
 		var text:FlxText = new FlxText(0, 0, 0, stringShit, fontSize);
 		text.setFormat(Paths.font("Coco-Sharp-Heavy-Italic-trial.ttf"), fontSize, FlxColor.WHITE, FlxTextAlign.LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		size = text.fieldWidth;
-		var bg:FlxSprite = new FlxSprite(fontSize/-2, fontSize/-2).makeGraphic(Math.floor(size + fontSize) + 20, Math.floor(text.height + fontSize), FlxColor.BLACK);
+		var bg:FlxSprite = new FlxSprite(fontSize/-2, fontSize/-2).makeGraphic(Math.floor(size + fontSize), Math.floor(text.height + fontSize), FlxColor.BLACK);
 		bg.alpha = 0.67;
 		add(bg);
 		add(text);
 		x -= size;
 
+		var file:String = Paths.txt(songName + '/' + 'info');
+		if(OpenFlAssets.exists(file)) { // info file fix?
+			stringShit = openfl.utils.Assets.getText(SUtil.getPath() + file);
+		}
+		else {
+			stringShit = 'NO BITCHES?'; // placeholder
+			// and yes, no bitches.
+			if (FlxG.random.bool(0.1)){
+				stringShit += '\nYES, NO BITCHES.';
+			}
+		}
 	}
 	public function tweenStartLololololololololololololololololololololollllmlllmlloololololololololololololololollololololollolololololllololololooloololoooloololoolooloollololllollllolookooilolololololololololololollolololololploololololololollollolllolololllololololololololololoololololllollolololololollollllololololololololololololooololololololol(){ //ok stop spamming lol pls
 		FlxTween.tween(this, {x: x + size + (fontSize/2)}, 1, {ease: FlxEase.cubeInOut, startDelay:0.5, onComplete:function(twn:FlxTween){
