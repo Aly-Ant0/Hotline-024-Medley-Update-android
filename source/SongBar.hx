8d146ab6e8c075434433908c52a928cc21506ad0
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2023-04-27 22:45:52 -0300

    update source/SongBar.hx

--- source/SongBar.hx
+++ source/SongBar.hx
@@ -1,50 +1,58 @@
 package;
 
-import flixel.FlxG;
+#if sys
+import sys.io.File;
+#end
+
+import lime.utils.Assets;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.FlxSprite;
 import flixel.group.FlxSpriteGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
-import openfl.utils.Assets;
 
 using StringTools;
 using flixel.util.FlxSpriteUtil;
 
-class SongBar extends FlxSpriteGroup // porra mateus so nao tava encontrando a 
+class TaskSong extends FlxSpriteGroup
 {
+	var meta:Array<Array<String>> = [];
 	var size:Float = 0;
-	var fontSize:Int = 42;
-	public var stringShit:String = "";
+	var fontSize:Int = 24;
 
-	public function new(x:Float, y:Float) {
-		super(x, y);
-		var songName:String = Paths.formatToSongPath(PlayState.SONG.song);
-		var text:FlxText = new FlxText(0, 0, 0, "", fontSize);
-		text.setFormat(Paths.font("Coco-Sharp-Heavy-Italic-trial.ttf"), fontSize, FlxColor.WHITE, FlxTextAlign.LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+	public function new(_x:Float, _y:Float, _song:String, ?_numberThing:Int = -1) {
+		super(_x, _y);
+
+		var pulledText:String = Assets.getText(Paths.txt(_song.toLowerCase().replace(' ', '-') + "/info"));
+		pulledText += '\n';
+
+		var text = new FlxText(0, 0, 0, "", fontSize);
+		text.setFormat(Paths.font("Coco-Sharp-Heavy-Italic-trial.ttf.ttf"), fontSize, FlxColor.WHITE, FlxTextAlign.LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+
+		text.text = pulledText;
+		text.updateHitbox();
+
 		size = text.fieldWidth;
-		var bg:FlxSprite = new FlxSprite(fontSize/-2, fontSize/-2).makeGraphic(Math.floor(size + fontSize), Math.floor(text.height + fontSize), FlxColor.BLACK);
-		bg.alpha = 0.67;
+
+		var bg = new FlxSprite(fontSize/-2, fontSize/-2).makeGraphic(Math.floor(size + fontSize), Std.int(text.height + 15), FlxColor.WHITE);
+		bg.alpha = 0.47;
+		text.text += "\n";
+
 		add(bg);
 		add(text);
-		x -= size;
 
-		var file:String = Paths.txt(songName + '/' + 'info');
-		if(Assets.exists(file)) { // info file fix?
-			stringShit = Assets.getText(SUtil.getPath() + file);
-		}
-		else {
-			stringShit = 'NO BITCHES?'; // placeholder
-			// and yes, no bitches.
-			if (FlxG.random.bool(0.1)){
-				stringShit += '\nYES, NO BITCHES.';
-			}
-		}
+		x -= size;
+		alpha = 0.00000001; 
 	}
-	public function tweenLol(){ //ok stop spamming lol pls
-		FlxTween.tween(this, {x: x + size + (fontSize/2)}, 1, {ease: FlxEase.cubeInOut, startDelay:0.5, onComplete:function(twn:FlxTween){
-			FlxTween.tween(this, {x: x - size}, 1, {ease: FlxEase.cubeInOut, startDelay: 5, onComplete: function(twn:FlxTween){ this.destroy(); }});
+
+	public function start(){
+		alpha = 1;
+
+		FlxTween.tween(this, {x: x + size + (fontSize/2)}, 1, {ease: FlxEase.quintInOut, onComplete: function(twn:FlxTween){
+			FlxTween.tween(this, {x: x - size - 50}, 1, {ease: FlxEase.quintInOut, startDelay: 2, onComplete: function(twn:FlxTween){ 
+				this.destroy(); 
+			}});
 		}});
 	}
 }
